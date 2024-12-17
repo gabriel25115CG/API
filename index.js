f09a50beb4504da1bd5bb7cf175e4ca227bd2eb3
gabriel <gabriel.chalmet@utbm.fr>
2024-12-17 10:14:18 +0000

    menage3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { validateEnv } from './utils/validateEnv.js'; 
-import { authenticateToken } from './middleware/authMiddleware.js'; 
+import { validateEnv } from './utils/validateEnv.js'; // Si tu as des variables d'env à valider
+import { authenticateToken } from './middleware/authMiddleware.js'; // Middleware d'authentification
 import authRoutes from './routes/authRoutes.js';
 import firestoreRoutes from './routes/firestoreRoutes.js';
 import { 
@@ -9,18 +9,68 @@ import {
   httpRequestDurationSeconds, 
   httpRequestsByStatus, 
   updateAvgResponseTime 
-} from './metrics.js'; 
-import client from 'prom-client'; 
+} from './metrics.js'; // Importer les métriques
+import client from 'prom-client'; // Client Prometheus
+import winston from 'winston'; // Importer Winston
 
+// Charger les variables d'environnement
 dotenv.config();
 
+// Valider les variables d'environnement
 validateEnv();
 
+// Configuration du logger avec Winston
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.Console({ format: winston.format.simple() }),
+    new winston.transports.File({ filename: 'api_logs.log' })
+  ],
+});
 
 // Créer l'application Express
 const app = express();
 app.use(express.json());
 
+// Middleware pour logger chaque requête et suivre les métriques
+app.use((req, res, next) => {
+  const { method, url } = req;
+  const timestamp = new Date().toISOString();
+  
+  
+  const route = req.route ? req.route.path : req.url;
+  const start = Date.now();
+
+  res.on('finish', () => {
+    // Incrémenter le compteur pour chaque requête HTTP
+    httpRequestsTotal.inc({
+      method: req.method,
+      route: route,
+      status: res.statusCode,
+    });
+
+    // Mesurer la durée de la requête
+    const duration = (Date.now() - start) / 1000; // Durée en secondes
+    httpRequestDurationSeconds.observe({
+      method: req.method,
+      route: route,
+    }, duration);
+
+    // Mettre à jour la durée moyenne des requêtes
+    updateAvgResponseTime(duration);
+
+    // Suivre les statuts des réponses HTTP
+    httpRequestsByStatus.inc({
+      status: res.statusCode.toString(),
+    });
+  });
+
+  next();
+});
 
 // Route de base
 app.get('/', (req, res) => {

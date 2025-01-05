493c9ecb8f7d97e9865ddabe2b26dcc1b74e0b39
gabriel <gabriel.chalmet@utbm.fr>
2025-01-05 18:33:38 +0000

    bug fix

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(express.json());
 
 // Configurer CORS pour autoriser toutes les origines
 const corsOptions = {
-  origin: 'http://heartsense.fr',  // Permet toutes les origines
+  origin: '*',  // Permet toutes les origines
   methods: 'GET,POST,PUT,DELETE',  // Méthodes autorisées
   allowedHeaders: 'Content-Type, Authorization',  // En-têtes autorisés
 };
